d56f9c001b17094dc22d37c710c435b428c77778
William Sorrentino <williamsorrentino@gmail.com>
2019-08-13 11:30:22 -0400

    coding the game logic

--- starter/app.js
+++ starter/app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 
 
@@ -20,7 +20,6 @@ document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
-
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
@@ -35,6 +34,20 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. update round score IF the rolled number was not a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
+    }
 });
 
 
